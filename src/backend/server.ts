4b21f18c14eaca3b567adf3c899451799f8e2425
r-zubkov <r.zubkov.it@gmail.com>
2026-02-16 14:22:04 +0300

    increased json limits

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -70,8 +70,8 @@
    }
  
    app.use(morgan('dev'));
-   app.use(express.json());
-   app.use(express.urlencoded({ extended: true }));
+   app.use(express.json({ limit: '5mb' }));
+   app.use(express.urlencoded({ extended: true, limit: '5mb' }));
    app.use(cookieParser());
    app.use(express.static(path.join(__dirname, '../../public')));
  
